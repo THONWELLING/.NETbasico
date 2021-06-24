2c8c7918d04fd6cb80743b607a2f1eb01e73882e
THONWELLING <thonwellingdani13@gmail.com>
2021-06-24 02:59:28 -0300

    commit inicial iniciando .NET

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = obterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o Nome do Aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a Nota do Aluno: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }else
+                        {
+                            throw new ArgumentException("Valor da Nota Deve Ser Decimal");
+                        }
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+                        //TODO : calcular média geral 
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            opcaoUsuario = obterOpcaoUsuario();
+        }
+
+        private static string obterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a Opção Desejada:");
+            Console.WriteLine("1- Inserir Novo Aluno");
+            Console.WriteLine("2- Listar Alunos");
+            Console.WriteLine("3- Calcular média Geral");
+            Console.WriteLine("x- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
